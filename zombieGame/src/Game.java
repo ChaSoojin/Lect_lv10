9674b3e28b821c9d26757f6f7fb78931462636d2
ChaSoojin <39975932+ChaSoojin@users.noreply.github.com>
2021-10-13 23:19:39 +0900

    좀비게임 프로젝트

---
+++ zombieGame/src/Game.java
@@ -0,0 +1,16 @@
+
+public class Game {
+	private static Game instance = new Game();
+	
+	private Game() {
+		
+	}
+	
+	public static Game getinstance() {
+		return instance;
+	}
+	
+	public void run() {
+		
+	}
+}
